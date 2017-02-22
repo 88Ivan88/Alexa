b2de99af4243a163ecdc5bb79c9e2bb6341a389f
88Ivan88 <libo-dalian@hotmail.com>
2017-02-23 01:04:49 +0800

    add team event  and team structure skills

--- src/index.js
+++ src/index.js
@@ -94,7 +94,7 @@ var handlers = {
             var speechOutput = this.t("PERSON_INFO_NOT_FOUND_MESSAGE");
             var repromptSpeech = this.t("PERSON_INFO_FOUND_REPROMPT");
             if (itemName) {
-                speechOutput += this.t("PERSON_INFO_FOUND_WITH_ITEM_NAME", itemName);
+                speechOutput += this.t("PERSON_INFO_FOUND_WITH_ITEM_NAME");
             } else {
                 speechOutput += this.t("PERSON_INFO_FOUND_WITHOUT_ITEM_NAME");
             }
@@ -129,59 +129,64 @@ var handlers = {
                 speechOutput = "sorry, currently we don't have the record for " + itemSlotName;
                 repromptSpeech = this.t("PERSON_INFO_REPEAT_MESSAGE");
             }
+        }else{
+            speechOutput = "sorry, currently we don't have the record for this person."
+            repromptSpeech = this.t("PERSON_INFO_REPEAT_MESSAGE");
         }
         this.emit(':ask', speechOutput, repromptSpeech);
     },
     'TeamEventsIntent': function () {
         var itemSlotTeamName = this.event.request.intent.slots.teamName;
-        var speechOutput = "";
-        var repromptSpeech = "";
+        var speechOutput = "sorry, currently I don't have the record for this team.";
+        var repromptSpeech = "sorry, currently I don't have the record for this team.";
         if (itemSlotTeamName && itemSlotTeamName.value) {
             itemSlotTeamName = itemSlotTeamName.value.toLowerCase();
-            var events = teamEvents.TEAM_EVENTS[itemSlotTeamName].events;
-            if(events instanceof Array && events.length > 0){
-                for(var i = 0 ; i< events.length ; i++){
-                    var eventNo = i+1;
-                    speechOutput += " the event " + eventNo+ " is :" + events[i] + ";";
+            speechOutput = "";
+            repromptSpeech = "";
+            if(teamEvents.TEAM_EVENTS[itemSlotTeamName] != undefined){
+                var events = teamEvents.TEAM_EVENTS[itemSlotTeamName].events;
+                if(events instanceof Array && events.length > 0){
+                    for(var i = 0 ; i< events.length ; i++){
+                        var eventNo = i+1;
+                        speechOutput += " the event " + eventNo+ " is :" + events[i] + ";";
+                    }
+                }else{
+                    speechOutput = "there are no big events from " + itemSlotTeamName + " team.";
                 }
-            }else{
-                speechOutput = "there are no big events from " + itemSlotTeamName + " team.";
+                repromptSpeech = "Try saying repeat."
             }
-            repromptSpeech = "Try saying repeat."
-        }else{
-            speechOutput = "sorry, currently I don't have the record for this team.";
-            repromptSpeech = "sorry, currently I don't have the record for this team.";
         }
         this.emit(':ask', speechOutput, repromptSpeech);
     },
     'TeamStructureIntent': function () {
         var itemSlotTeamName = this.event.request.intent.slots.teamName;
-        var speechOutput = "";
-        var repromptSpeech = "";
+        var speechOutput = "sorry, currently I don't have the record for this team.";
+        var repromptSpeech = "sorry, currently I don't have the record for this team.";
         if (itemSlotTeamName && itemSlotTeamName.value) {
             itemSlotTeamName = itemSlotTeamName.value.toLowerCase();
-            var po = teamEvents.TEAM_EVENTS[itemSlotTeamName].productOwner;
-            var scrumMaster = teamEvents.TEAM_EVENTS[itemSlotTeamName].scrumMaster;
-            var members = teamEvents.TEAM_EVENTS[itemSlotTeamName].teamMembers;
-            speechOutput = "In "+ itemSlotTeamName +" team the product owner is " + po +", the scrum master is " + scrumMaster + ", " +
-                "the team members";
-            if(members instanceof Array && members.length >1){
-                speechOutput +=" are ";
-                for (var i = 0; i < members.length; i++) {
-                    var member = members[i];
-                    speechOutput += member;
-                    if (i < members.length - 1) {
-                        speechOutput += ",";
+            speechOutput = "";
+            repromptSpeech = "";
+            if(teamEvents.TEAM_EVENTS[itemSlotTeamName] != undefined) {
+                var po = teamEvents.TEAM_EVENTS[itemSlotTeamName].productOwner;
+                var scrumMaster = teamEvents.TEAM_EVENTS[itemSlotTeamName].scrumMaster;
+                var members = teamEvents.TEAM_EVENTS[itemSlotTeamName].teamMembers;
+                speechOutput = "In " + itemSlotTeamName + " team the product owner is " + po + ", the scrum master is " + scrumMaster + ", " +
+                    "the team members";
+                if (members instanceof Array && members.length > 1) {
+                    speechOutput += " are ";
+                    for (var i = 0; i < members.length; i++) {
+                        var member = members[i];
+                        speechOutput += member;
+                        if (i < members.length - 1) {
+                            speechOutput += ",";
+                        }
                     }
+                } else {
+                    speechOutput += " is ";
+                    speechOutput += members[0];
                 }
-            }else{
-                speechOutput += " is ";
-                speechOutput += members[0];
+                repromptSpeech = "Try saying repeat."
             }
-            repromptSpeech = "Try saying repeat."
-        }else{
-            speechOutput = "sorry, currently I don't have the record for this team.";
-            repromptSpeech = "sorry, currently I don't have the record for this team.";
         }
         this.emit(':ask', speechOutput, repromptSpeech);
     },
@@ -225,7 +230,7 @@ var languageStrings = {
             "STOP_MESSAGE": "oh my god !!!! I only want to find a person who I can talk to. Is it really difficult ? Am I wrong ? oh no ! Bye Bye !",
             "PERSON_INFO_REPEAT_MESSAGE": "Try saying repeat.",
             "PERSON_INFO_NOT_FOUND_MESSAGE": "I\'m sorry, I currently don\'t know ",
-            "PERSON_INFO_FOUND_WITH_ITEM_NAME": "the person info for %s skill. ",
+            "PERSON_INFO_FOUND_WITH_ITEM_NAME": "the person info for that skill. ",
             "PERSON_INFO_FOUND_WITHOUT_ITEM_NAME": "that skill. ",
             "PERSON_INFO_FOUND_REPROMPT": "What else can I help with?",
             "PERSON_INFO_FOUND_WITH_CONTRACT_INFO": "%s\'s %s"
